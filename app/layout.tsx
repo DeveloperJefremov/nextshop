0719d8f552eba8d4753e41dc43b57b7f867109ce
Artjoms Jefremovs <developerjefremov@gmail.com>
2025-03-26 17:26:30 +0200

    ModeToggle component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/assets/styles/globals.css';
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
+import { ThemeProvider } from 'next-themes';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -18,8 +19,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={`${inter.className}  antialiased`}>{children}</body>
+		<html lang='en' suppressHydrationWarning>
+			<body className={`${inter.className}  antialiased`}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='light'
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
--- components/(shared)/header/index.tsx
+++ components/(shared)/header/index.tsx
@@ -4,6 +4,7 @@ import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import ModeToggle from './mode-toggle';
 
 const Header = () => {
 	return (
@@ -23,7 +24,8 @@ const Header = () => {
 						</span>
 					</Link>
 				</div>
-				<div className='space-x-2 '>
+				<div className='flex items-center  space-x-2 '>
+					<ModeToggle />
 					<Button asChild variant='ghost'>
 						<Link href='/cart'>
 							<ShoppingCart /> Cart
