c10281915967b10dd3ce493aa70a3fe8f5de7850
Artjoms Jefremovs <developerjefremov@gmail.com>
2025-03-31 12:21:04 +0200

    Added CredentialsSignInForm  component

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -10,6 +10,7 @@ import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import CredentialsSignInForm from './credentials-signin-form';
 
 export const metadata: Metadata = {
 	title: 'SignIn',
@@ -34,7 +35,9 @@ const SignInPage = () => {
 						Sign in to your account
 					</CardDescription>
 				</CardHeader>
-				<CardContent className='space-y-4'></CardContent>
+				<CardContent className='space-y-4'>
+					<CredentialsSignInForm />
+				</CardContent>
 			</Card>
 		</div>
 	);
