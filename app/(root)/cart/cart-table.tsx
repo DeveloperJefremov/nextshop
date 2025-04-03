6d512a05276b774f33d8067d5bea4ce8dfaf6aaf
Artjoms Jefremovs <developerjefremov@gmail.com>
2025-04-03 10:09:49 +0200

    Used shadcn Table component for CartTable component

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -1,6 +1,18 @@
 'use client';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table';
 import { useToast } from '@/hooks/use-toast';
-import { Cart } from '@/types';
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
+import { formatCurrency } from '@/lib/utils';
+import { Cart, CartItem } from '@/types';
 import { ArrowRightIcon, LoaderIcon, MinusIcon, PlusIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -12,18 +24,104 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
 	const { toast } = useToast();
 	const [isPending, startTransition] = useTransition();
 	return (
-		<div>
+		<>
 			<h1 className='py-4 h2-bold'>Shopping Cart</h1>
 			{!cart || cart.items.length === 0 ? (
 				<div>
 					Cart is empty. <Link href='/'>Go Shopping</Link>
 				</div>
 			) : (
-				<div className='grid md:grid-cols-4 md:gap-5'>
-					<div className='overflow-x-auto md:-col-span-3'>Table</div>
+				<div
+					className='grid
+			'
+					//	 md:grid-cols-4 md:gap-5'
+				>
+					<div className='overflow-x-auto md:-col-span-3'>
+						<Table>
+							<TableHeader>
+								<TableRow>
+									<TableHead>Item</TableHead>
+									<TableHead className='text-center'>Quantity</TableHead>
+									<TableHead className='text-right'>Price</TableHead>
+								</TableRow>
+							</TableHeader>
+							<TableBody>
+								{cart.items.map(item => (
+									<TableRow key={item.slug}>
+										<TableCell>
+											<Link
+												href={`/product/${item.slug}`}
+												className='flex items-center'
+											>
+												<Image
+													src={item.image}
+													alt={item.name}
+													width={50}
+													height={50}
+												/>
+												<span className='px-2'>{item.name}</span>
+											</Link>
+										</TableCell>
+										<TableCell className='flex-center gap-2'>
+											<Button
+												disabled={isPending}
+												variant='outline'
+												type='button'
+												onClick={() =>
+													startTransition(async () => {
+														const res = await removeItemFromCart(
+															item.productId
+														);
+
+														if (!res.success) {
+															toast({
+																variant: 'destructive',
+																description: res.message,
+															});
+														}
+													})
+												}
+											>
+												{isPending ? (
+													<LoaderIcon className='w-4 h-4 animate-spin' />
+												) : (
+													<MinusIcon className='w-4 h-4' />
+												)}
+											</Button>
+											<span>{item.qty}</span>
+											<Button
+												disabled={isPending}
+												variant='outline'
+												type='button'
+												onClick={() =>
+													startTransition(async () => {
+														const res = await addItemToCart(item);
+
+														if (!res.success) {
+															toast({
+																variant: 'destructive',
+																description: res.message,
+															});
+														}
+													})
+												}
+											>
+												{isPending ? (
+													<LoaderIcon className='w-4 h-4 animate-spin' />
+												) : (
+													<PlusIcon className='w-4 h-4' />
+												)}
+											</Button>
+										</TableCell>
+										<TableCell className='text-right'>${item.price}</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</div>
 				</div>
 			)}
-		</div>
+		</>
 	);
 };
 
