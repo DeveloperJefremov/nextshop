38f64d25f10f50c974eb456b770d93c100330a11
Artjoms Jefremovs <developerjefremov@gmail.com>
2025-04-03 11:00:37 +0200

    cart subtotal

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -12,7 +12,7 @@ import {
 import { useToast } from '@/hooks/use-toast';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { formatCurrency } from '@/lib/utils';
-import { Cart, CartItem } from '@/types';
+import { Cart } from '@/types';
 import { ArrowRightIcon, LoaderIcon, MinusIcon, PlusIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -32,7 +32,7 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
 				</div>
 			) : (
 				<div
-					className='grid
+					className='grid  
 			'
 					//	 md:grid-cols-4 md:gap-5'
 				>
@@ -119,6 +119,30 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
 							</TableBody>
 						</Table>
 					</div>
+					<Card>
+						<CardContent className='p-4 gap-4'>
+							<div className='pb-3 text-xl'>
+								Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+								<span className='font-bold'>
+									{formatCurrency(cart.itemsPrice)}
+								</span>
+							</div>
+							<Button
+								className='w-full'
+								disabled={isPending}
+								onClick={() =>
+									startTransition(() => router.push('/shipping-address'))
+								}
+							>
+								{isPending ? (
+									<LoaderIcon className='w-4 h-4 animate-spin' />
+								) : (
+									<ArrowRightIcon className='w-4 h-4' />
+								)}{' '}
+								Proceed to Checkout
+							</Button>
+						</CardContent>
+					</Card>
 				</div>
 			)}
 		</>
