992e9d58bfcc86f228486058edb17115af5d287b
Artjoms Jefremovs <developerjefremov@gmail.com>
2025-04-11 14:33:36 +0300

    Update User action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -14,6 +14,7 @@ import {
 	shippingAddressSchema,
 	signInFormSchema,
 	signUpFormSchema,
+	updateUserSchema,
 } from '../validators';
 
 //Sign in user with credentials
@@ -225,5 +226,27 @@ export async function deleteUser(id: string) {
 			success: false,
 			message: formatError(error),
 		};
+	}
+}
+
+// Update a user
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+	try {
+		await prisma.user.update({
+			where: { id: user.id },
+			data: {
+				name: user.name,
+				role: user.role,
+			},
+		});
+
+		revalidatePath('/admin/users');
+
+		return {
+			success: true,
+			message: 'User updated successfully',
+		};
+	} catch (error) {
+		return { success: false, message: formatError(error) };
 	}
 }
