8de997f585c2c630e63aaee04413e3c1fb027008
Artjoms Jefremovs <developerjefremov@gmail.com>
2025-04-15 11:52:41 +0300

    Added review list component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from '@/auth';
 import AddToCart from '@/components/(shared)/product/add-to-cart';
 import ProductImages from '@/components/(shared)/product/product-images';
 import ProductPrice from '@/components/(shared)/product/product-price';
@@ -8,12 +9,15 @@ import { getProductBySlug } from '@/lib/actions/product.actions';
 
 import { notFound } from 'next/navigation';
 import React from 'react';
+import ReviewList from './review-list';
 const ProductDetailsPage = async (props: {
 	params: Promise<{ slug: string }>;
 }) => {
 	const { slug } = await props.params;
 	const product = await getProductBySlug(slug);
 	if (!product) notFound();
+	const session = await auth();
+	const userId = session?.user?.id;
 
 	const cart = await getMyCart();
 
@@ -84,6 +88,14 @@ const ProductDetailsPage = async (props: {
 						</Card>
 					</div>
 				</div>
+			</section>
+			<section className='mt-10'>
+				<h2 className='h2-bold'>Customer Reviews</h2>
+				<ReviewList
+					userId={userId || ''}
+					productId={product.id}
+					productSlug={product.slug}
+				/>
 			</section>
 		</>
 	);
