f4f5fe4eb03559262228bbe9b31a09a8d5129562
Artjoms Jefremovs <developerjefremov@gmail.com>
2025-04-15 15:31:22 +0300

    Added stripe payment success page

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -3,16 +3,18 @@ import { getOrderById } from '@/lib/actions/order.actions';
 import { ShippingAddress } from '@/types';
 import { Metadata } from 'next';
 import { notFound, redirect } from 'next/navigation';
-import React from 'react';
 import Stripe from 'stripe';
 import OrderDetailsTable from './order-details-table';
 
 export const metadata: Metadata = {
 	title: 'Order Details',
-	description: 'Order details page',
 };
 
-const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
+const OrderDetailsPage = async (props: {
+	params: Promise<{
+		id: string;
+	}>;
+}) => {
 	const { id } = await props.params;
 
 	const order = await getOrderById(id);
